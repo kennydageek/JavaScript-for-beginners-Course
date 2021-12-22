129a8d2abb0e543993cab5879a25befa1ea0d145
Ajibade Kehinde David <ajibadekehinde1@gmail.com>
2021-12-22 23:31:18 +0000

    Start conditions, roll dice, display dice

--- 06. The Dice Game/script.js
+++ 06. The Dice Game/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const btnNew = document.querySelector('.btn--new');
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for a rolled 1:
+  if (dice !== 1) {
+    // Add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //  CHANGE LATER
+  } else {
+    // switch to next player
+  }
+});
