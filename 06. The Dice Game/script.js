46dbd759861a6ce73ed35395804391581fbec19b
Ajibade Kehinde David <ajibadekehinde1@gmail.com>
2021-12-23 00:48:17 +0000

    Resetting the game

--- 06. The Dice Game/script.js
+++ 06. The Dice Game/script.js
@@ -13,15 +13,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -31,6 +42,7 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
+init();
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
@@ -65,7 +77,7 @@ btnHold.addEventListener('click', function () {
 
     // 2. Check if player's score is >= 100
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -82,3 +94,5 @@ btnHold.addEventListener('click', function () {
     // Switch to the next player
   }
 });
+
+btnNew.addEventListener('click', init);
