18d86e30bcf202ab8805cd8d5b86c12b074dbf2d
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2021-12-28 22:43:27 +0000

    Scoping in Practice

---
+++ 07. Behind the Scenes/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Working with The Scope Chain
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
