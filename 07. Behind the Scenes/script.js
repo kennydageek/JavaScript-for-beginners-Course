8b903e5f862f2dbc962deca4c033a5236bf16e79
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-04 21:27:28 +0000

    introduction to primitive and reference type

--- 07. Behind the Scenes/script.js
+++ 07. Behind the Scenes/script.js
@@ -109,7 +109,7 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
-*/
+
 
 const jonas = {
   firstName: 'Jonas',
@@ -153,3 +153,23 @@ var addArrow = (a, b) => {
   return a + b;
 };
 addArrow(2, 5, 8);
+*/
+
+// PRIMITIVES VS OBJECTS {PRIMITIVE VS REFERENCE TYPES}
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+
+console.log('Friend:', friend);
+console.log('Me', me);
