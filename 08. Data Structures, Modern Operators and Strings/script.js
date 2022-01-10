676639b6eb591082a9b5ebdde6e230c7daae3c0f
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-10 12:35:02 +0000

    Enhanced Object Literals done

--- 08. Data Structures, Modern Operators and Strings/script.js
+++ 08. Data Structures, Modern Operators and Strings/script.js
@@ -455,7 +455,22 @@ team1 < team2 && console.log('Team1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
 */
 
-// LOOPING ARRAYS: THE FOR OF LOOP
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[5]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day ${2 * 3}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
 const restaurant = {
   name: 'Classico Italiano',
@@ -464,47 +479,35 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // ES6 enhanced object literals
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredinets) {
+  orderPizza(mainIngredient, ...otherIngredinets) {
     console.log(mainIngredient);
     console.log(otherIngredinets);
   },
 };
+
+// ENHANCED OBJECT LITERALS
+
+/*
+// LOOPING ARRAYS: THE FOR OF LOOP
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
@@ -514,3 +517,4 @@ for (const [i, el] of menu.entries()) {
   console.log(`${i + 1}: ${el}`);
 }
 // console.log(...menu.entries());
+*/
