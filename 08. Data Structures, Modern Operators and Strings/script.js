d5b26fc834a8dbaecc42300c860ec889775a46c0
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-10 00:47:52 +0000

    Shortcircuitng OR and AND logical Operator

--- 08. Data Structures, Modern Operators and Strings/script.js
+++ 08. Data Structures, Modern Operators and Strings/script.js
@@ -261,6 +261,7 @@ const restaurant = {
     console.log(otherIngredinets);
   },
 };
+/*
 
 // 1. Destructuring
 
@@ -299,3 +300,34 @@ add(...x);
 
 restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mushrooms');
+*/
+
+// Short Ciruitting && and ||
+
+console.log('..... OR .....');
+// Use ANY data, return ANY data type, short circuiting
+console.log(3 || 'jonas');
+console.log('' || 'jonas'); // 'jonas'
+console.log(true || 0); // true
+console.log(undefined || null); //
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('.... AND ....');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
