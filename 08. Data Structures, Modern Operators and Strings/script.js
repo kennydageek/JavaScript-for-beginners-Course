d50fe721f6e290e9009d2da46c1ca2346ec3e2b2
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-11 22:36:52 +0000

    Update script.js

--- 08. Data Structures, Modern Operators and Strings/script.js
+++ 08. Data Structures, Modern Operators and Strings/script.js
@@ -504,6 +504,38 @@ const restaurant = {
   },
 };
 
+// SETS
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+console.log(ordersSet);
+console.log(new Set('Jonas'));
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+let staffUnique = new Set(staff);
+
+staffUnique = [...staffUnique];
+console.log(staffUnique);
+
+/*
 // Looping Objects: Object Keys, Values, and Entries.
 
 // Looping Propety Names
@@ -530,7 +562,7 @@ const entries = Object.entries(openingHours);
 for (const [key, { open, close }] of entries) {
   console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
-/*
+
 // Optional Chaining(.?)
 console.log(restaurant.openingHours.mon.open);
 console.log(restaurant.openingHours.mon?.open);
