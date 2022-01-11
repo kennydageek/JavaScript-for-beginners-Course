431b483d15db3220ea0d2c8d0b2cfac278af69e7
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-11 23:30:44 +0000

    Maps Fundamentals Done

--- 08. Data Structures, Modern Operators and Strings/script.js
+++ 08. Data Structures, Modern Operators and Strings/script.js
@@ -504,6 +504,38 @@ const restaurant = {
   },
 };
 
+//  Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :[');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+/*
 // SETS
 const ordersSet = new Set([
   'Pasta',
@@ -539,7 +571,6 @@ console.log(
 
 console.log(new Set('jonasschmedtmann').size);
 
-/*
 // Looping Objects: Object Keys, Values, and Entries.
 
 // Looping Propety Names
