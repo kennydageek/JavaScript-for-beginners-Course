eafd868ff1db32d27545cb034917543b117c6ebd
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-12 00:10:32 +0000

    Maps: Iteration done

--- 08. Data Structures, Modern Operators and Strings/script.js
+++ 08. Data Structures, Modern Operators and Strings/script.js
@@ -504,6 +504,43 @@ const restaurant = {
   },
 };
 
+// Maps: Iteration
+
+const question = new Map([
+  ['question', 'What is the best programing language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct :D'],
+  [false, 'Try again :['],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Iterating Maps
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log(question.entries());
+console.log(question.keys());
+console.log(question.values());
+/*
 //  Maps: Fundamentals
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
@@ -535,7 +572,7 @@ console.log(rest);
 console.log(rest.size);
 
 console.log(rest.get(arr));
-/*
+
 // SETS
 const ordersSet = new Set([
   'Pasta',
