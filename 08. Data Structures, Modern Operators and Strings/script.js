af92af6bd35ee64202d7b92333f02d27514bd593
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-14 14:10:23 +0000

    Additional Exercise Done!

--- 08. Data Structures, Modern Operators and Strings/script.js
+++ 08. Data Structures, Modern Operators and Strings/script.js
@@ -1,8 +1,28 @@
 'use strict';
 
 // Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Delayed Departure from FAO to TXL (11h25)
+// .........Arrival: from BRU to FAO (11h45)
+// ...Delayed Arrival from HEL to FAO(12h05)
+// ....... Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '<<' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(19);
+  console.log(output);
+}
+
 /*
 // Data needed for first part of the section
 const restaurant = {
@@ -453,7 +473,7 @@ printGoals(...game.scored);
 
 team1 < team2 && console.log('Team1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
-*/
+
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
@@ -553,7 +573,7 @@ planesInLine(5);
 planesInLine(3);
 planesInLine(12);
 
-/*
+
 
 // Working with Strings: Part 2
 
