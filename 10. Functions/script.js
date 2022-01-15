654813d215f9c17a59010b1e560f662c8848eeb5
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-15 23:38:14 +0000

    Default Parameters lecture done!

--- 10. Functions/script.js
+++ 10. Functions/script.js
@@ -1,1 +1,28 @@
 'use strict';
+/*
+// Default Parameters
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('LH123', 2, 8001);
+createBooking('LH123', 2);
+
+createBooking('LH123', undefined, 1000);
+*/
