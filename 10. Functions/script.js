869d84790d6897fa93478189ac0fc3e983e4b8bd
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-16 22:50:20 +0000

    Exercise Done!

--- 10. Functions/script.js
+++ 10. Functions/script.js
@@ -196,3 +196,14 @@ const addVAT = addTax.bind(null, 0.23);
 
 console.log(addVAT(100));
 console.log(addVAT(23));
+
+// Challenge
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
