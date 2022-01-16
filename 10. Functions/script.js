5ed173bdc8ef38a4d803d71744cdf0ea9ac53504
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-16 00:00:22 +0000

    Passing Arguements: Value vs Reference

--- 10. Functions/script.js
+++ 10. Functions/script.js
@@ -26,3 +26,35 @@ createBooking('LH123', 2);
 
 createBooking('LH123', undefined, 1000);
 */
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = `Mr. ${passenger.name}`;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
