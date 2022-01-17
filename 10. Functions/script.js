62b5776e5af32bfcbc5c2e7be2660e3c9e49d584
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-17 14:41:02 +0000

    Added my solution to the challenge

--- 10. Functions/script.js
+++ 10. Functions/script.js
@@ -248,7 +248,7 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 
 // [5,2,3]
 // [1,5,3,9,6,1]
-*/
+
 
 // Immediately Invoked Function Expressions
 
@@ -260,3 +260,34 @@ runOnce();
 (function () {
   console.log('This will never run again');
 })();
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0,0,0,0]. More int he next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    let input = Number(prompt(`${this.question}\n${this.options.join('\n')}`));
+    if (typeof input === 'number' && input < this.answers.length) {
+      this.answers[input]++;
+      this.displayResults();
+      this.displayResults('string');
+    }
+    console.log(this.answers);
+  },
+  displayResults: function (type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(',')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
