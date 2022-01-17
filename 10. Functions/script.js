2fc0597019387a35f53497af7aa7d76254b55c66
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-17 22:23:05 +0000

    Closure examples

--- 10. Functions/script.js
+++ 10. Functions/script.js
@@ -311,3 +311,30 @@ booker();
 booker();
 
 console.dir(booker);
+
+// More Closure Examples
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+// Reassigning f function
+h();
+f();
+
+console.dir(f);
