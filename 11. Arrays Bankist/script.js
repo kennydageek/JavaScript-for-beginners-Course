045389bdbc189b714d1b99d601fe69738f86a78f
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-18 23:48:30 +0000

    Concat Done

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -95,3 +95,8 @@ arr = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['j', 'i', 'h', 'g', 'f'];
 console.log(arr2.reverse());
 console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+// console.log([...arr, arr2]); An alternative is to use the spread operator
