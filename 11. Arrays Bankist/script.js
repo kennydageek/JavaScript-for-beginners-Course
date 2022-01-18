cb790a05f7ff1c97c26b30b8c7a4822ded15cb74
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-18 23:31:07 +0000

    The Splice Method Done!

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -84,3 +84,8 @@ console.log(arr.slice(2, 4));
 console.log(arr.slice(-2));
 console.log(arr.slice(1, -2));
 console.log(arr.slice()); // Creates shallow copy like the spread operator
+
+// SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
