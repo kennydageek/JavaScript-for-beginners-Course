b3fd621330325ee512df2353bd340ddd37916cd9
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-18 23:15:37 +0000

    Slice Method Done!

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -74,3 +74,13 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// The slice Method
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+console.log(arr.slice()); // Creates shallow copy like the spread operator
