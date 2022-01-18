ce318a30a5431ea07a5947c6260734c17b153eb8
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-18 00:20:43 +0000

    Closure Examples Finished

--- 10. Functions/script.js
+++ 10. Functions/script.js
@@ -338,3 +338,17 @@ h();
 f();
 
 console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
