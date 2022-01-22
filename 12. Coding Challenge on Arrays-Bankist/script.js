cbce1ebc8b13797f1262e1a6457cfa295736ba40
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-22 05:32:10 +0100

    First Task of Challenge 2

--- 12. Coding Challenge on Arrays-Bankist/script.js
+++ 12. Coding Challenge on Arrays-Bankist/script.js
@@ -1,3 +1,4 @@
+/*
 const checkDogs = function (dogsJulia, dogsKate) {
   // Task 1
   let dogsJuliaCopy = dogsJulia.slice(0);
@@ -20,3 +21,20 @@ const checkDogs = function (dogsJulia, dogsKate) {
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+
+const calcAverageHumanAge = function (ages) {
+  // Task 1
+  const humanAge = ages.map(function (age, i) {
+    if (age <= 2) {
+      return age * 2;
+    } else {
+      return 16 + age * 2;
+    }
+  });
+  console.log(humanAge);
+
+  // Task 2
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
