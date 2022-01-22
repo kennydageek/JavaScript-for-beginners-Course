3ecc5262185957cac32c0af588f283255e1e4272
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-22 02:21:52 +0100

    Map Method in practice

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -91,7 +91,7 @@ displayMovements(account1.movements);
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -176,3 +176,30 @@ currenciesUnique.forEach(function (value, key, set) {
   console.log(`${key}: ${value}`);
 });
 */
+
+// Converting with Maps
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+// simplifying Maps callback function
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+// Converting with ForOf
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescriptions);
