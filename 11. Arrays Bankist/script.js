e7c2bc5a9d995d8204881f56b20ee896653ef51a
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-22 03:28:13 +0100

    Filter Method in Practice

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -92,7 +92,6 @@ const createUsernames = function (accs) {
 };
 
 createUsernames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -217,3 +216,18 @@ const movementsDescriptions = movements.map(
 
 console.log(movementsDescriptions);
 */
+
+// Filter Method
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+// Achieving Filter with ForOf loop
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
