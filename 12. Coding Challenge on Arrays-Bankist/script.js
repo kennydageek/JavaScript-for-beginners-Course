75a373b087cfacdbbacb2f111c2bdb1f511604f2
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-22 17:13:23 +0100

    Coding Challenge 3 done

--- 12. Coding Challenge on Arrays-Bankist/script.js
+++ 12. Coding Challenge on Arrays-Bankist/script.js
@@ -21,7 +21,7 @@ const checkDogs = function (dogsJulia, dogsKate) {
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
-*/
+
 
 const calcAverageHumanAge = function (ages) {
   // Task 1
@@ -32,6 +32,22 @@ const calcAverageHumanAge = function (ages) {
 
   // Task 3
   const average = adult.reduce((acc, age) => acc + age, 0) / adult.length;
+  return average;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+*/
+
+// Coding Challenge 3
+
+const calcAverageHumanAge = (ages) => {
+  const average =
+    ages
+      .map((age) => (age <= 2 ? age * 2 : 16 + age * 4))
+      .filter((age) => age >= 18)
+      .reduce((acc, age) => acc + age, 0) / ages.length;
+
   return average;
 };
 
