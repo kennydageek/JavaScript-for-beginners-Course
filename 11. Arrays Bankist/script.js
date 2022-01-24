50387ba31a2b5f488e47d051a22491fdfde47fbf
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-24 15:09:24 +0100

    Login Implementation Updated

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -85,20 +85,20 @@ const calcDisplayBalance = function (movements) {
   labelBalance.textContent = `${balance}€`;
 };
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
       return int >= 1;
     })
@@ -143,7 +143,7 @@ btnLogin.addEventListener('click', function (e) {
     // Display Balance
     calcDisplayBalance(currentAccount.movements);
     // Display Summary
-    calcDisplaySummary(currentAccount.movements);
+    calcDisplaySummary(currentAccount);
   }
 });
 
