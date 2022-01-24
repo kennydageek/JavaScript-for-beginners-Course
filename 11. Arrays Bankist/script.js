091ebd10c96880a9db6d0a606b3cce4e2d4fe75a
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-24 16:40:45 +0100

    Close account feature implemented

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -179,6 +179,28 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputClosePin.value = inputCloseUsername.value = '';
+  labelWelcome.textContent = `Log in to get started`;
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
