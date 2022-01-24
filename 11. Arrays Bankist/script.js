ef2c20b2695c5cceb587bdc2e72524df4aa9fa78
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-24 14:09:21 +0100

    The Find Method

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -102,8 +102,6 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
-
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
@@ -283,7 +281,7 @@ const max = movements.reduce((acc, mov) => {
   }
 }, movements[0]);
 console.log(max);
-*/
+
 
 const eurToUsd = 1.1;
 
@@ -294,3 +292,19 @@ const totalDepositsUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDepositsUSD);
+*/
+
+// The Find Method
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+
+for (const [i, acc] of accounts.entries()) {
+  if (accounts[i].owner === 'Jessica Davis') console.log(accounts[i]);
+  else continue;
+}
