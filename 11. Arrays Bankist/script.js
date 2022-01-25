6452b95b4c88e8e895af07f2298096e740404ec5
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-25 11:39:56 +0100

    Having a Separate Callback

--- 11. Arrays Bankist/script.js
+++ 11. Arrays Bankist/script.js
@@ -420,3 +420,9 @@ console.log(anyDeposits);
 // EVERY
 console.log(movements.every(mov => mov > 0));
 console.log(account4.movements.every(mov => mov > 0));
+
+// Separate Callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
