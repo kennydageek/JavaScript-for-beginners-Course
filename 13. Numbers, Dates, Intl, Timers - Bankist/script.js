69e8db9b64b61646127547de683fcc3be44e1675
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-27 13:26:01 +0100

    Creating and Working with Dates

--- 13. Numbers, Dates, Intl, Timers - Bankist/script.js
+++ 13. Numbers, Dates, Intl, Timers - Bankist/script.js
@@ -332,7 +332,7 @@ console.log(PI);
 console.log(Number('230000')); // 230000
 console.log(Number('230_000')); // Doesn't work
 console.log(Number.parseInt('230_000')); // 230
-*/
+
 
 // Working with BigInt
 
@@ -341,4 +341,40 @@ console.log(Number.MAX_SAFE_INTEGER); // The biggest number JS can safely repres
 
 console.log(28484848887484387348734870808704280887954n);
 
-// Operations
+
+
+// Creating dates
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Thu Jan 27 2022 12:52:15 GMT+0100'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(2037, 10, 19, 15, 23, 5)
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 1000));
+
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+
+console.log(future.toISOString());
+console.log(future.getTime());
+console.log(new Date(2142253380000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+*/
