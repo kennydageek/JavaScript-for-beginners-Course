1f2be868e14c3d751452a7d7769902a025523f1f
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-01 12:53:14 +0100

    Intersection API applied to reveal elements

--- 14. Advanced DOM Bankist/script.js
+++ 14. Advanced DOM Bankist/script.js
@@ -173,7 +173,31 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   threshold: 0,
   rootMargin: `-${navHeight}px`,
 });
+
 headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 //////////////////////////
 /////////////////////////
@@ -185,7 +209,7 @@ headerObserver.observe(header);
 // console.log(document.body);
 
 // const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
+// const allSections = document.querySelectorAll('.section');
 // console.log(allSections);
 
 document.getElementById('section--1');
