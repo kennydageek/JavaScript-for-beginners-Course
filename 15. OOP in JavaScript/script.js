f7c4047faa4473648ed3f8bbcd4012bda75fc8f4
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-07 16:10:32 +0100

    Prototypal Inheritance on Built-in Objects

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -48,5 +48,27 @@ console.log(Person.prototype.isPrototypeOf(Person));
 Person.prototype.species = 'Homo Sapiens';
 console.log(jonas.species, matilda.species);
 
-console.log(jonas.hasownProperty('firstName'));
-console.log(jonas.hasownProperty('species'));
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+//Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 5, 4, 8, 7, 6, 1, 1, 2]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
