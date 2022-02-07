d4a0af979e29d5217f710903285636fd2dd7f4c3
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-07 14:52:32 +0100

    constructor functions and the new operator

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
