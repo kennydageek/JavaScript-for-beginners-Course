1744a2613bc940f46f26964edbba32d293ff53d4
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-08 09:46:18 +0100

    ES6 Classes lectures

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -73,7 +73,7 @@ console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
-*/
+
 
 // Coding Challenge #1;
 
@@ -104,3 +104,40 @@ BMW.accelerate();
 BMW.brake();
 Mercedes.accelerate();
 Mercedes.brake();
+*/
+
+// const PersonCl = class {
+
+// }
+
+// Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
