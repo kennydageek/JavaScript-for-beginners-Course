50d5808b6c3ea20d30a9f5c40d9ea115b8b4a566
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-08 15:52:31 +0100

    Task 2 Challenge #2

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -234,12 +234,26 @@ class CarCl {
     this.speed -= 10;
     console.log(`${this.make} going at ${this.speed}km/h`);
   }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
 }
 
 const bmw = new CarCl('BMW', 120);
 const mercedes = new CarCl('Mercedes', 95);
+const ford = new CarCl('Ford', 120);
 
-console.log(bmw);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
 
 bmw.accelerate();
 bmw.accelerate();
