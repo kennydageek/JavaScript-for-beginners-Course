9a90939af766ea154da9d149ace5ce79f7696488
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-09 16:08:54 +0100

    Another Class example

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -408,7 +408,7 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 
 martha.introduce();
 martha.calcAge();
-*/
+
 
 // Inheritance Between Classes: Object.create
 
@@ -439,3 +439,48 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+// Another Class Example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currrency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
