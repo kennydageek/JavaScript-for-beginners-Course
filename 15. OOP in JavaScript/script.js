4a9920c903d9bdcec8a63c2d52ef6acbe35ae959
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-09 15:19:53 +0100

    Inheritance between classes using Object.create

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -344,7 +344,7 @@ tesla.chargeBattery(90);
 tesla.brake();
 tesla.accelerate();
 tesla.chargeBattery(100);
-*/
+
 
 class PersonCl {
   constructor(fullName, birthYear) {
@@ -408,3 +408,34 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 
 martha.introduce();
 martha.calcAge();
+*/
+
+// Inheritance Between Classes: Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
