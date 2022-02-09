04f5fafe9ef03f837eb7dcf0004db887379e7d37
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-09 14:10:41 +0100

    Coding Challenge #3

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -297,3 +297,50 @@ console.log(mike.__proto__.__proto__);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+
+// Coding Challenge 3
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+// Task 1
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+
+  console.log(`${this.make} going at ${this.speed} charged to ${this.charge}`);
+};
+
+EV.prototype.accelerate = function (speed) {
+  this.speed += 20;
+  this.charge--;
+
+  console.log(
+    `${this.make} going at ${this.speed}km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.chargeBattery(90);
+tesla.brake();
+tesla.accelerate();
+tesla.chargeBattery(100);
