89b327677a6f46c6bbec62f0a7b70b7631cc0f44
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-10 16:49:42 +0100

    Protected properties and Methods (Encapsulation)

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -447,26 +447,33 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currrency = currency;
-    this.pin = pin;
-    this.movements = [];
+    this._pin = pin;
+
+    // protected property
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -482,5 +489,7 @@ acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
+console.log(acc1.pin);
