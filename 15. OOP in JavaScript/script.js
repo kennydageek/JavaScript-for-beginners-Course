82babe984a7632a9379aa505a04c5a8aac6e31cd
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-10 17:25:09 +0100

    Chaining Methods

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -472,16 +472,19 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+    return this;
   }
 
   requestLoan(val) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
+      return this;
     }
   }
 
@@ -509,3 +512,8 @@ console.log(acc1);
 // console.log(acc1.#movements);
 // console.log(acc1.#pin);
 console.log(acc1._approveLoan(100));
+
+// Chaining methods
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+
+console.log(acc1.getMovements());
