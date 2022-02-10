9f295dba808f2ebae7b838a1d4a9e81695d67d73
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-10 17:18:02 +0100

    Encapsulation using the new class fields proposal

--- 15. OOP in JavaScript/script.js
+++ 15. OOP in JavaScript/script.js
@@ -444,32 +444,38 @@ jay.calcAge();
 // Another Class Example
 
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currrency = currency;
-    this._pin = pin;
+    this.#pin = pin;
 
     // protected property
-    this._movements = [];
-    this.locale = navigator.language;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  // 3) Public methods
+
   // Public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
-  }
-  _approveLoan(val) {
-    return true;
   }
 
   requestLoan(val) {
@@ -477,6 +483,12 @@ class Account {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
   }
 }
 
@@ -488,8 +500,12 @@ const acc1 = new Account('Jonas', 'EUR', 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+// acc1.approveLoan(1000);
 console.log(acc1.getMovements());
 
 console.log(acc1);
-console.log(acc1.pin);
+// console.log(acc1.pin);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+console.log(acc1._approveLoan(100));
