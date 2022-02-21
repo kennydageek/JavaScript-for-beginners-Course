ca7573c38530ad7d824c16a7edcac329bc1dd095
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-02-21 12:51:02 +0100

    Consuming Promises

--- 17. Asynchronous/script.js
+++ 17. Asynchronous/script.js
@@ -98,4 +98,23 @@ getCountryAndNeighbour('usa');
 */
 
 const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
-console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('portugal');
