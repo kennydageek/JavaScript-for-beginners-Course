19bd4fdbeb2c5aa4408b2410e2ffb00b1c25588d
Kehinde Ajibade <ajibadekehinde1@gmailcom>
2022-03-29 17:02:52 +0100

    coding challenge #3 part 1

--- 17. Asynchronous/script.js
+++ 17. Asynchronous/script.js
@@ -398,7 +398,7 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('portugal', 'canada', 'tanzania');
-*/
+
 
 // Promise.race
 (async function () {
@@ -449,3 +449,50 @@ Promise.any([
 ])
   .then(res => console.log(res))
   .catch(err => console.error(err));
+*/
+
+// Coding Challenge #3
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+// Coding Challenge #3
+
+// Part 1
+const loadNPause = async function () {
+  try {
+    const img1 = await createImage('img/img-1.jpg');
+    await wait(5);
+    img1.style.display = 'none';
+
+    const img2 = await createImage('img/img-2.jpg');
+    await wait(5);
+    img2.style.display = 'none';
+
+    const img3 = await createImage('img/img-3.jpg');
+    await wait(5);
+  } catch {
+    err => console.error('Image not found');
+  }
+};
+loadNPause();
