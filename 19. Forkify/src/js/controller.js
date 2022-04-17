11122e6a9248879c838feb11e8843f2cc09d94ce
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-04-17 20:33:01 +0100

    hashchange events and load events

--- 19. Forkify/src/js/controller.js
+++ 19. Forkify/src/js/controller.js
@@ -34,13 +34,18 @@ const renderSpinner = function(parentEl) {
 
 const showRecipe = async function() {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if(!id) return;
+
     // 1) Loading recipe
 
     renderSpinner(recipeContainer);
 
    const res = await fetch(
-    //  `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca10`
-  `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
+    
+  `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
 
 
    const data = await res.json();
@@ -160,3 +165,7 @@ const showRecipe = async function() {
   }
 }
 showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe))
+// window.addEventListener('hashchange', showRecipe)
+// window.addEventListener('load', showRecipe);
